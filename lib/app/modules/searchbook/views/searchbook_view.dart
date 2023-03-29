de50258235cdec8072a29e1b33f27ccde8adc1af
Widiskel <waputra16@gmail.com>
2023-03-29 23:29:02 +0700

    search screen

--- lib/app/modules/searchbook/views/searchbook_view.dart
+++ lib/app/modules/searchbook/views/searchbook_view.dart
@@ -107,6 +107,23 @@ class SearchbookView extends GetView<SearchbookController> {
                                 Image.network(
                                   controller.listBook[index].image!,
                                   fit: BoxFit.fill,
+                                  loadingBuilder: (BuildContext context,
+                                      Widget child,
+                                      ImageChunkEvent? loadingProgress) {
+                                    if (loadingProgress == null) return child;
+                                    return Center(
+                                      child: CircularProgressIndicator(
+                                        value: loadingProgress
+                                                    .expectedTotalBytes !=
+                                                null
+                                            ? loadingProgress
+                                                    .cumulativeBytesLoaded /
+                                                loadingProgress
+                                                    .expectedTotalBytes!
+                                            : null,
+                                      ),
+                                    );
+                                  },
                                 ),
                                 const SizedBox(
                                   width: 10.0,
